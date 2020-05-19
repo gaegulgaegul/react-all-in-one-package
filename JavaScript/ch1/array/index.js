ae9f41fc227c3baf4a84ea8f38a7ff0bd8b75d21
gaegulgaegul <dla9721@gmail.com>
2020-05-20 06:51:40 +0900

    배열

---
+++ JavaScript/ch1/array/index.js
@@ -0,0 +1,14 @@
+const array = [1,2,3,4,5]
+console.log(array);
+
+/********************************************/
+const objects = [
+    {name:'멍멍이'},
+    {name:'야옹이'}
+];
+console.log(objects);
+console.log(objects[0]);
+console.log(objects[1]);
+
+objects.push({name:'멍뭉이'});
+console.log(objects);
