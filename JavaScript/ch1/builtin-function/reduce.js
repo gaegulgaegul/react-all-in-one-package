ff0113895a93b0781acfbfbfa7a92f503fe7432f
gaegulgaegul <dla9721@gmail.com>
2020-05-22 06:07:05 +0900

    reduce

--- JavaScript/ch1/builtin-function/reduce.js
+++ JavaScript/ch1/builtin-function/reduce.js
@@ -7,3 +7,14 @@ let avg = numbers.reduce((accumulator, current, index, array) => {
     return accumulator + current;
 }, 0);
 console.log(avg);
+
+/**************************************************************************/
+const alphabets = ['a','a','a','b','c','c','d','e',];
+const count = alphabets.reduce((acc, current) => {
+    if(acc[current]) {
+        acc[current] += 1;
+    } else {
+        acc[current] = 1;
+    }
+    return acc;
+}, {});
